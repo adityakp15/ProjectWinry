a1900ccb69aa801bbcfd3a7d98049187aca328b7
adityakp15 <aditponnu@gmail.com>
2020-08-15 17:49:02 +0530

    Added function to process users requests

--- winry.py
+++ winry.py
@@ -6,6 +6,7 @@ from gtts import gTTS
 from selenium import webdriver
 
 num = 1
+shutdown = ["bye","nothing","sleep","shut up","exit"]
 
 def winry_speaks(output):
     global num
@@ -26,26 +27,51 @@ def speech_to_text():
         #winry_speaks("What would you like me to do ? ")
         print("Speak....")
         r.adjust_for_ambient_noise(source)
-        audio = r.listen(source,phrase_time_limit=3)
-    print("Gotcha ! ")
+        audio = r.listen(source,phrase_time_limit=2)
+
     #winry_speaks("Okay, thats enough now.")
 
     try:
+        winry_speaks("Gotcha ! ")
         text = r.recognize_google(audio,language="en-us")
         print("You :",text)
         print("Processing....")
         return text
     except:
-        winry_speaks("I couldn't understand anything, you fool.")
+        winry_speaks("I didn't quite get you, please repeat. ")
         return 0
 
-if __name__=="__main__":
-    try:
-        winry_speaks("Hi my name is Winry, what is yours ? ")
-        name = speech_to_text()
+def get_name():
+    name = speech_to_text()
+    if(name != 0):
         winry_speaks("Hello " + name )
-    except:
-        winry_speaks("I didn't quite get you ")
+        return name
+    else:
+        get_name()
+
+def process_request(request):
+    try:
+        
+
+
+if __name__=="__main__":
+
+    winry_speaks("Hi my name is Winry, what is yours ? ")
+    name = get_name()
+
+    while(1):
+        winry_speaks("What can i do for you today, "+name)
+        request = speech_to_text()
+
+        if(request == 0):
+            winry_speaks("I didn't quite get you, please repeat. ")
+            continue
+        if(str(request.lower()) in shutdown):
+            winry_speaks("Okay, I will "+str(request)+", "+name)
+            break
+        #print(request)
+        process_request(request)
+    
 
 
 
